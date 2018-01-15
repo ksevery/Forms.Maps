b7649263a6d8d835182be231d11cf9237c7276e6
KSevery <konstantin.severy@fourth.com>
2018-01-15 11:39:03 +0200

    Add project files.

---
+++ Forms.Maps/Forms.Maps/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Forms.Maps.Views;
+using Xamarin.Forms;
+
+namespace Forms.Maps
+{
+	public partial class App : Application
+	{
+
+		public App ()
+		{
+			InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
